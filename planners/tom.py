5e2e193de1f8fef4e9553de2b31a0a0eab7429bf
aridThought <garbedCrustacean@gmail.com>
2025-05-11 13:44:31 -0500

    gfffsd

--- planners/tom.py
+++ planners/tom.py
@@ -50,66 +50,47 @@ def backValue(node: MimiNode):
 def AnotherAnotherStar(s, grid, start, end, avoid):
     rows, cols = len(grid), len(grid[0])
     directions = [(0,1),(1,1),(1,0),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1), (0,0)]
-    directions2 = [(0,1),(1,1),(1,0),(-1,1),(-1,0),(-1,-1),(0,-1),(1,-1)]
     
     pathWeight = {start: 0}
     parent = {start: None}
     
 
-    open = [start]
+    open = []
+    nodeVal = {}
+    node = start
 
-    found = False
-    while not found and len(open) > 0:
-        node = None
-        for newNode in open:
-            if node is None or pathWeight[newNode] <= pathWeight[node]:
-                node = newNode
-
-        if node == end:
-            found = True
-            break
-
-        nodeVal = {}
-        open.remove(node)
-
-        for dir in directions:
-            newNode = (node[0] + dir[0], node[1] + dir[1])
-            nodeVal[newNode] = hurst(newNode, end,avoid,rows,cols,grid) + dist(newNode, end)
-            if newNode not in list(pathWeight.keys()):
-                pathWeight[newNode] = pathWeight[node]
-                #if 0 <= newNode[0] < rows and 0 <= newNode[1] < cols and grid[newNode[0]][newNode[1]] == 0:
-                parent[newNode] = node
-                open.append(newNode)
-            #if newNode == end:
-                #found = True                 
+    for dir in directions:
+        newNode = (node[0] + dir[0], node[1] + dir[1])
+        nodeVal[newNode] = hurst(newNode, end,avoid,rows,cols,grid) + dist(newNode, end)
+        if newNode not in list(pathWeight.keys()):
+            pathWeight[newNode] = pathWeight[node]
+            parent[newNode] = node
+            open.append(newNode)
+        #if newNode == end:
+            #found = True                 
         
         
-        for d in directions:
-            nodeToBe = (node[0] + d[0], node[1] + d[1])
-            
-            for i in range(3):
-                newNode = None
-                if(i == 0):
-                    newNode = (node[0] + -d[1], node[1] + d[0])
-                elif(i == 1):
-                    newNode = nodeToBe
-                else:
-                    newNode = (node[0] + d[1], node[1] + -d[0])
-            
-                pathWeight[nodeToBe] += nodeVal[newNode]*s.probabiliry[i]
-             
-             
+    for d in directions:
+        nodeToBe = (node[0] + d[0], node[1] + d[1])
+        
+        for i in range(3):
+            newNode = None
+            if(i == 0):
+                newNode = (node[0] + -d[1], node[1] + d[0])
+            elif(i == 1):
+                newNode = nodeToBe
+            else:
+                newNode = (node[0] + d[1], node[1] + -d[0])
+        
+            pathWeight[nodeToBe] += nodeVal[newNode]*s.probabiliry[i]
 
-    if found:
-            path = []
-            parentNode = end
-            while parentNode is not None:
-                path.append(parentNode)
-                if parent[parentNode] is None:
-                    break
-                parentNode = parent[parentNode]
-            return path[::-1]
-    return None
+
+    for newNode in open:
+        if pathWeight[newNode] <= pathWeight[node]:
+            node = newNode
+             
+    return node - newNode
+
 
 def hurst(current, end, pursuer, rows, cols, grid):
     if tuple(current) == tuple(pursuer) or 0 > current[0] >= rows or 0 > current[1] >= cols or (0 <= current[0] < rows and 0 <= current[1] < cols and grid[current[0]][current[1]] == 0):
@@ -194,7 +175,7 @@ class PlannerAgent:
         #        target = pursued_plan[i]
 
 
-        our_plan = AnotherAnotherStar(self, world, temp, temp2, temp3)
+        return AnotherAnotherStar(self, world, temp, temp2, temp3)
 
         act = our_plan[1] - current
 
